eeca9430e64c27f3e7bf9ef7cd2bb5780dbf8519
irkallacz <mottl.jakub.janov@gmail.com>
2019-04-03 13:41:50 +0200

    Calendar test

---
+++ tests/CalendarPresenterTest.php
@@ -0,0 +1,28 @@
+<?php //SignPresenterTest.php
+
+require __DIR__ . '/bootstrap.php';
+
+/**
+ * @testCase
+ */
+final class CalendarPresenterTest extends \Tester\TestCase
+{
+
+	use \Testbench\TPresenter;
+
+	/**
+	 * @throws \Nette\Application\BadRequestException
+	 */
+	public function testRenderDefault()
+	{
+		$this->checkAction('Calendar:default');
+	}
+
+	public function testRenderDefaultPassword()
+	{
+		$this->checkAction('Calendar:default', ['password' => 'iZ2LrcLnQ9']);
+	}
+
+}
+
+(new CalendarPresenterTest())->run();
